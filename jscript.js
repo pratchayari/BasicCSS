41264e274eb404e4437da5fe353b76285ca36671
pratchayari <aokisawa@gmail.com>
2023-11-14 15:20:07 +0700

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,12 +1,24 @@
-fetch(rawGitUrl)
-.then(response => response.text())
-.then(data => {
-    // Split the content into lines
-    const lines = data.split('\n');
-
-    // Format the lines with line numbers
-    const formattedCode = lines.map((line, index) => `${index + 1}: ${line}`).join('\n');
-
-    document.getElementById('raw-code').innerText = formattedCode;
-})
-.catch(error => console.error('Error fetching raw file:', error));
+// jscript.js
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const codeElement = document.getElementById('raw-code');
+  
+    // Fetch the raw content of the file
+    fetch(rawGitUrl)
+      .then(response => response.text())
+      .then(data => {
+        // Set the content of the code element
+        codeElement.textContent = data;
+  
+        // Highlight the code using Prism.js
+        Prism.highlightElement(codeElement);
+  
+        // Add line numbers using the line-numbers plugin
+        Prism.plugins.lineNumbers.initialize({
+          // You can customize the options here if needed
+          // See: https://prismjs.com/plugins/line-numbers/
+        });
+      })
+      .catch(error => console.error('Error fetching raw file:', error));
+  });
+  
